2d8599c6da2eaba8bd960300568d7117b9e97911
Sufiya Ahmed Ansari <165552041+Sufiyaansari08@users.noreply.github.com>
2025-10-25 19:58:40 +0530

    Implementing interface

---
+++ 25.Interface.dart
@@ -0,0 +1,39 @@
+
+// Interface
+
+void main() {
+
+	var tv = Television();
+	tv.volumeUp();
+	tv.volumeDown();
+}
+
+class Remote {
+
+	void volumeUp() {
+		print("______Volume Up from Remote_______");
+	}
+
+	void volumeDown() {
+		print("______Volume Down from Remote_______");
+	}
+}
+
+
+
+// Here Remote and AnotherClass acts as Interface
+class Television implements Remote {
+
+	void volumeUp() {
+//		super.volumeUp();       // Not allowed to call super while implementing a class as Interface
+		print("______Volume Up in Television_______");
+	}
+
+	void volumeDown() {
+		print("______Volume Down in Television_______");
+	}
+
+	void justAnotherMethod() {
+		print("Some code");
+	}
+}
